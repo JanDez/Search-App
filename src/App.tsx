19bcbecb3748514ca8f74880179f0d97a347e372
Jaifran Hernandez <jaifranjose@gmail.com>
2025-01-16 14:15:34 -0400

    Feat: initialize app with React Query to setup pre fetch data and theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from './context/ThemeProvider';
 import Home from './pages/Home';
+import { fetchUsers } from './services/api';
+import { useEffect } from 'react';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+      gcTime: 1000 * 60 * 30,
+      refetchOnWindowFocus: false,
+      refetchOnMount: false
+    },
+  },
+});
 
 function App() {
+  useEffect(() => {
+    queryClient.prefetchQuery({
+      queryKey: ['users'],
+      queryFn: fetchUsers,
+    });
+  }, []);
+
   return (
     <ThemeProvider>
-      <Home />
+      <QueryClientProvider client={queryClient}>
+        <Home />
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
